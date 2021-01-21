863c106e4699840258f6c154fc864a3d2f131447
Сергей Политаев <ronin@sonikelf.ru>
2021-01-22 02:43:59 +0300

    moved commands to bin

---
+++ src/bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import program from 'commander';
+import genDiff from './index.js';
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  
+  .option('-f, --format', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action(genDiff(filepath1, filepath2))
+
+program.parse(process.argv);
--- src/commands.js
+++ src/commands.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import program from 'commander';
-import genDiff from './index.js';
+import genDiff from '../index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  
   .option('-f, --format', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action(genDiff(filepath1, filepath2))

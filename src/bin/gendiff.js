b21004879b3c5ea6e4762954120738b2b3f60baf
Сергей Политаев <ronin@sonikelf.ru>
2021-01-26 03:55:10 +0300

    fixed style problems

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
-import genDiff from '../index.js';
+import genDiff from '../index';
 
 program
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')  
+  .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
     genDiff(filepath1, filepath2);
-  })
+  });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
-
 const genDiff = (path1, path2) => {
-  const readFile = (filePath) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filePath), "utf-8"));
+  const readFile = (filePath) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf-8'));
   const object1 = readFile(path1);
   const object2 = readFile(path2);
   const object1keys = Object.keys(object1);
@@ -14,29 +13,29 @@ const genDiff = (path1, path2) => {
 
   const statusCollection = uniqueKeys.reduce((result, key) => {
     if (!_.has(object1, key)) {
-      result.push({ key, 'value': object2[key], 'status': '+' });
+      result.push({ key, value: object2[key], status: '+' });
     }
     if (!_.has(object2, key)) {
-      result.push({ key, 'value': object1[key], 'status': '-' });
+      result.push({ key, value: object1[key], status: '-' });
     }
     if (_.has(object1, key) && _.has(object2, key)) {
       if (object1[key] !== object2[key]) {
-        result.push({ key, 'value': object1[key], 'status': '-' });
-        result.push({ key, 'value': object2[key], 'status': '+' });
+        result.push({ key, value: object1[key], status: '-' });
+        result.push({ key, value: object2[key], status: '+' });
       }
       if (object1[key] === object2[key]) {
-        result.push({ key, 'value': object1[key], 'status': ' ' });
+        result.push({ key, value: object1[key], status: ' ' });
       }
     }
     return result;
   }, []);
-  const sortedStatusCollection = _.sortBy(statusCollection, [function(o) { return o.key; }]);
+  const sortedStatusCollection = _.sortBy(statusCollection, [(o) => o.key]);
   const result = sortedStatusCollection.reduce((res, item) => {
     res.push(`${item.status} ${item.key}: ${item.value}`);
     return res;
-  }, [])
+  }, []);
   console.log(`{\n${result.join('\n')}\n}`);
-}
+};
 
 export default genDiff;
 

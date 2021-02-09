00983665dffe88876f47e88920c0f93db4a507d9
Сергей Политаев <ronin@sonikelf.ru>
2021-02-09 09:45:18 +0300

    fixed stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,40 @@
-// import _ from 'lodash';
+import _ from 'lodash';
+
+const convertToString = (data, givenDepth) => {
+  if (_.isObject(data)) {
+    const iter = (object, depth) => {
+      const entries = Object.entries(object);
+      return entries.map(([key, value]) => {
+        if (_.isObject(value)) {
+          return `${'    '.repeat(depth)}${key}: {\n${iter(value, depth + 1)}\n${'    '.repeat(depth)}}`;
+        }
+        return `${'    '.repeat(depth)}${key}: ${value}`;
+      });
+    };
+    return `{\n${iter(data, givenDepth + 1).join('\n')}\n${'    '.repeat(givenDepth)}}`;
+  }
+  return data;
+};
 
 const stylish = (tree) => {
   const iter = (node, depth) => {
     const result = node.flatMap((item) => {
       switch (item.status) {
         case 'added':
-          return `${'  '.repeat(depth + 1)}+ ${item.key}: ${item.value}`;
+          return `${' '.repeat(depth * 4 - 2)}+ ${item.key}: ${convertToString(item.value, depth)}`;
         case 'deleted':
-          return `${'  '.repeat(depth + 1)}- ${item.key}: ${item.value}`;
+          return `${' '.repeat(depth * 4 - 2)}- ${item.key}: ${convertToString(item.value, depth)}`;
         case 'unchanged':
-          return `${'  '.repeat(depth + 1)}  ${item.key}: ${item.value}`;
+          return `${'    '.repeat(depth)}${item.key}: ${convertToString(item.value, depth)}`;
         case 'node':
-          return `${'  '.repeat(depth + 1)}  ${item.key}: {\n${iter(item.children, depth + 2)}\n  ${'  '.repeat(depth + 1)}}`;
+          return `${'    '.repeat(depth)}${item.key}: {\n${iter(item.children, depth + 1)}\n${'    '.repeat(depth)}}`;
         default:
           throw new Error('Unknown value status');
       }
     });
     return result.join('\n');
   };
-  return `{\n${iter(tree, 0)}\n}`;
+  return `{\n${iter(tree, 1)}\n}`;
 };
 
 export default stylish;
--- src/getComparisonData.js
+++ src/getComparisonData.js
@@ -5,24 +5,18 @@ const getComparisonData = (file1Data, file2Data) => {
   const file2keys = Object.keys(file2Data);
   const uniqueKeys = _.sortBy(_.union(file1keys, file2keys));
   return uniqueKeys.reduce((result, key) => {
-    if (!_.has(file1Data, key)) {
+    if (_.isObject(file1Data[key]) && _.isObject(file2Data[key])) {
+      const children = getComparisonData(file1Data[key], file2Data[key]);
+      result.push({ key, status: 'node', children });
+    } else if (!_.has(file1Data, key)) {
       result.push({ key, value: file2Data[key], status: 'added' });
-    }
-    if (!_.has(file2Data, key)) {
+    } else if (!_.has(file2Data, key)) {
       result.push({ key, value: file1Data[key], status: 'deleted' });
-    }
-    if (_.has(file1Data, key) && _.has(file2Data, key)) {
-      if (_.isEqual(file1Data[key], file2Data[key])) {
-        result.push({ key, value: file1Data[key], status: 'unchanged' });
-      }
-      if (_.isObject(file1Data[key]) && _.isObject(file2Data[key])) {
-        const children = getComparisonData(file1Data[key], file2Data[key]);
-        result.push({ key, status: 'node', children });
-      }
-      if (!_.isEqual(file1Data[key], file2Data[key])) {
-        result.push({ key, value: file1Data[key], status: 'deleted' });
-        result.push({ key, value: file2Data[key], status: 'added' });
-      }
+    } else if (_.isEqual(file1Data[key], file2Data[key])) {
+      result.push({ key, value: file1Data[key], status: 'unchanged' });
+    } else {
+      result.push({ key, value: file1Data[key], status: 'deleted' });
+      result.push({ key, value: file2Data[key], status: 'added' });
     }
     return result;
   }, []);

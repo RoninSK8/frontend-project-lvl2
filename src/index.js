e7826544b6d4090c5687960f1d44fa753f1ad0fe
Сергей Политаев <ronin@sonikelf.ru>
2021-02-16 04:52:08 +0300

    refactor: renamed funcs and moved them to the module level

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import parse from './parsers.js';
 import format from './formatters/index.js';
 import getComparisonData from './getComparisonData.js';
 
-const genDiff = (path1, path2, formatter = 'stylish') => {
-  const normaliseFilePath = (filePath) => path.resolve(process.cwd(), filePath);
-  const getFile = (filePath) => fs.readFileSync(normaliseFilePath(filePath), 'utf-8');
-  const getFileFormat = (file) => path.extname(file).toLowerCase();
-  const file1Data = parse(getFile(path1), getFileFormat(path1));
-  const file2Data = parse(getFile(path2), getFileFormat(path2));
+const normaliseFilePath = (filePath) => path.resolve(process.cwd(), filePath);
+const getData = (filePath) => fs.readFileSync(normaliseFilePath(filePath), 'utf-8');
+const getFileFormat = (file) => path.extname(file).toLowerCase().slice(1);
 
-  return format(getComparisonData(file1Data, file2Data), formatter);
+const genDiff = (path1, path2, formatter = 'stylish') => {
+  const data1 = parse(getData(path1), getFileFormat(path1));
+  const data2 = parse(getData(path2), getFileFormat(path2));
+  return format(getComparisonData(data1, data2), formatter);
 };
 
 export default genDiff;
